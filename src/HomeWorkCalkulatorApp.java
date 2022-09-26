9fecdcc2304c8df24cb89c253203668c7ca65d8b
k.kosteniuk <konstantinkostenyuk@gmail.com>
2022-09-26 18:06:37 +0300

    Changed Calc

--- src/HomeWorkCalkulatorApp.java
+++ src/HomeWorkCalkulatorApp.java
@@ -3,13 +3,74 @@ import java.util.Scanner;
 public class HomeWorkCalkulatorApp {
 
     public static void main(String[] args) {
-        System.out.printf("Calkulator ON%n+ add, - subtract, * multiply, / divide.%n");
+        calcOn();
+    }
+
+    public static void calcOn () {
+        double firstNumber = getNumber();
+        String operation = getOperation();
+        double secondNumber = getNumber();
+        double result = calc(firstNumber, secondNumber, operation);
+        System.out.println("Result: " + result);
+    }
+
+    public static double calc(double num1, double num2, String operation) {
+        return switch (operation) {
+            case "+" -> add(num1, num2);
+            case "-" -> sub(num1, num2);
+            case "*" -> mul(num1, num2);
+            case "/" -> div(num1, num2);
+            default -> Double.NaN;
+        };
+    }
+
+    public static double getNumber() {
         Scanner sc = new Scanner(System.in);
-        double firstNumber = sc.nextDouble();
-        double caseCalk = sc.nextDouble();
-        double secondNumber = sc.nextDouble();
-        System.out.println(firstNumber);
-        System.out.println(caseCalk);
-        System.out.println(secondNumber);
+        System.out.println("Enter a number:");
+        if (sc.hasNextDouble()) {
+            return sc.nextDouble();
+        } else {
+            System.out.println("Incorrect! Try again...");
+            return getNumber();
+        }
+    }
+
+    public static String getOperation() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("""
+                Select operation number:
+                + - add
+                - - subtract
+                * - multiply
+                / - divide
+                """);
+        String operation = sc.nextLine();
+        if (sc.nextLine() == "+" || sc.nextLine() == "-" || sc.nextLine() == "*" || sc.nextLine() == "/") {
+            return operation;
+        } else {
+            System.out.println("Incorrect option! Try again...");
+            return getOperation();
+        }
+    }
+
+    public static double add(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public static double sub(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    public static double mul(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    public static double div(double num1, double num2) {
+        if (num2 != 0) {
+            return num1 / num2;
+        } else {
+            System.out.println("You can't divide by zero!");
+            return Double.NaN;
+        }
     }
 }
