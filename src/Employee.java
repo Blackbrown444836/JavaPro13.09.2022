cda5d2d85b32f706229c33254ab52b13773d10f5
k.kosteniuk <konstantinkostenyuk@gmail.com>
2022-09-30 10:30:56 +0300

    To do encapsulations code.

--- src/Employee.java
+++ src/Employee.java
@@ -1,49 +1,37 @@
 public class Employee {
-    private String nameEmployee;
-    private String posotionEmployee;
-    private String emailEmployee;
-    private String contactPhoneNumberEmployee;
+    private  String nameEmployee;
+    private  String posotionEmployee;
+    private  String emailEmployee;
+    private  String contactPhoneNumberEmployee;
     private int ageEmployee;
 
+    public Employee(String nameEmployee, String posotionEmployee, String emailEmployee,
+                    String contactPhoneNumberEmployee, int ageEmployee) {
+        this.nameEmployee = nameEmployee;
+        this.posotionEmployee = posotionEmployee;
+        this.emailEmployee = emailEmployee;
+        this.contactPhoneNumberEmployee = contactPhoneNumberEmployee;
+        if (ageEmployee <0) return;
+        this.ageEmployee = ageEmployee;
+    }
 
     public String getNameEmployee() {
         return nameEmployee;
-    }
-
-    public void setNameEmployee(String nameEmployee) {
-        this.nameEmployee = nameEmployee;
     }
 
     public String getPosotionEmployee() {
         return posotionEmployee;
     }
 
-    public void setPosotionEmployee(String posotionEmployee) {
-        this.posotionEmployee = posotionEmployee;
-    }
-
     public String getEmailEmployee() {
         return emailEmployee;
-    }
-
-    public void setEmailEmployee(String emailEmployee) {
-        this.emailEmployee = emailEmployee;
     }
 
     public String getContactPhoneNumberEmployee() {
         return contactPhoneNumberEmployee;
     }
 
-    public void setContactPhoneNumberEmployee(String contactPhoneNumberEmployee) {
-        this.contactPhoneNumberEmployee = contactPhoneNumberEmployee;
-    }
-
     public int getAgeEmployee() {
         return ageEmployee;
-    }
-
-    public void setAgeEmployee(int ageEmployee) {
-        if (ageEmployee < 0) return;
-        this.ageEmployee = ageEmployee;
     }
 }
--- src/purchasing_department/SameName.java
+++ src/purchasing_department/SameName.java
@@ -1,6 +1,5 @@
 package purchasing_department;
 
-
 public class SameName {
 
 }
