ccd11f0aca53fc3ac12b1cc847275e955ad8aac9
k.kosteniuk <konstantinkostenyuk@gmail.com>
2022-10-05 19:30:26 +0300

    Initial commit.

--- src/Cat.java
+++
@@ -1,8 +0,0 @@
-public class Cat extends Animal{
-    public static int countCat = 0;
-    public Cat(String nameAnimal, double runDistans) {
-        super(nameAnimal, runDistans);
-        countCat++;
-        System.out.println("This cat " + nameAnimal + " runed " + moveRun(setRunDistansAnimalLimit(200),runDistans) + "m");
-    }
-}
--- src/Dog.java
+++
@@ -1,9 +0,0 @@
-public class Dog extends Animal {
-    public static int countDog = 0;
-    public Dog (String nameAnimal, double runDistans, double swimDistans) {
-        super(nameAnimal, runDistans);
-        countDog++;
-        System.out.println("This dog " + nameAnimal + " runed " + moveRun(setRunDistansAnimalLimit(500),runDistans) + "m" +
-                " and swimed " + swimMove(setSwimDistansAnimalLimit(10), swimDistans) + "m");
-    }
-}
