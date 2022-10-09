a19d7a64e96c60c6b04f55ffed31f47db987e209
k.kosteniuk <konstantinkostenyuk@gmail.com>
2022-10-09 22:05:21 +0300

    Fixed a bug in the code, according to which I did not refer to the parent class, but for some reason to the classes personally. Also removed duplication of output to the console.

--- src/HomeAnimals.java
+++ src/HomeAnimals.java
@@ -1,13 +1,10 @@
 public class HomeAnimals {
     public static void main(String[] args) {
-        Cat murka = new Cat("Murka", 154.25);
-        Dog bobik = new Dog("Bobik", 365, 9.87);
-        Cat markiz = new Cat("Markiz", 130.21);
-        Dog barsic = new Dog("Barsic", 800.21, 2);
-        System.out.println(murka);
-        System.out.println(bobik);
-        System.out.println(markiz);
-        System.out.println(barsic);
+        Animal murka = new Cat("Murka", 154.25);
+        Animal bobik = new Dog("Bobik", 565, 19.87);
+        Animal markiz = new Cat("Markiz", 130.21);
+        Animal barsic = new Dog("Barsic", 300.21, 2);
+
         System.out.println("Total annimals: " + (Cat.countCat + Dog.countDog) + ", cats: " + Cat.countCat + ", dog: "
                 + Dog.countDog + ".");
     }
