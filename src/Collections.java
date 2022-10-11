794d1e6e529bc67bfdf5b9411cb49c913e2906c1
k.kosteniuk <konstantinkostenyuk@gmail.com>
2022-10-11 11:29:20 +0300

    First initial.

---
+++ src/Collections.java
@@ -0,0 +1,32 @@
+import java.util.List;
+
+public class Collections {
+    public static void main(String[] args) {
+
+    }
+
+    public static void countOccurance (){
+        List<String> arr = new java.util.LinkedList<>();
+        arr.add("chicken");
+        arr.add("pasta");
+        arr.add("lemon");
+        arr.add("bananas");
+        arr.add("soda");
+        arr.add("tomatoes");
+        arr.add("potato");
+        arr.add("zucchini");
+        arr.add("tea");
+        arr.add("cosmetics");
+        arr.add("sponges");
+        arr.add("paper");
+        arr.add("corn");
+        arr.add("paper");
+        arr.add("apricots");
+        arr.add("bread");
+        arr.add("cleaner");
+        arr.add("rice");
+        arr.add("vegetables");
+        arr.add("salt");
+
+    }
+}
